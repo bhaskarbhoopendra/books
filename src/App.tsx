7d46f01864b1937a26ecfaeca29fd45736e8e8b4
bhaskarbhoopendra <bhaskarbhoopendra8@gmail.com>
2022-12-13 20:49:18 +0530

    React hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import Footer from "./compoundComponents/Footer";
 import Navbar from "./compoundComponents/Navbar";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 function App() {
+  //react hooks
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <Navbar />
+      <h1>
+        You have clicked the button <span className="text-muted">{count}</span>{" "}
+        number of time{" "}
+      </h1>
+      <Button
+        onClick={() => setCount(count + 1)}
+        className="mx-5 my-5"
+        variant="success"
+      >
+        Increase Count
+      </Button>
       <Footer />
     </div>
   );
--- src/components/Aboutus.tsx
+++ src/components/Aboutus.tsx
@@ -1,7 +1,20 @@
 import Footer from "../compoundComponents/Footer";
 import Navbar from "../compoundComponents/Navbar";
+import axios from "axios";
+import { useState } from "react";
 
 function Aboutus() {
+  const [store, setStore] = useState([]);
+
+  axios
+    .get(`https://jsonplaceholder.typicode.com/users`)
+    .then((response) => {
+      setStore(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  console.log({ store });
   return (
     <div>
       <Navbar />
